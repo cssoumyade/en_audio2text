b075333a9942245e60d4b72e56138ca76e80aac2
Soumya De <cssoumyade@gmail.com>
2021-03-25 15:00:54 +0530

    changing setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="en_audio2text-soumyade", # Replace with your own username
-    version="0.0.1.1",
+    version="0.0.1.2",
     author="Soumya De",
     author_email="cs.soumyade@gmail.com",
     description="small wrapper around speech recognition package",
