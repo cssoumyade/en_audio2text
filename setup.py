b803ae658e26e4d645afa7f487cd0f7976207bbb
Soumya De <cssoumyade@gmail.com>
2021-03-25 13:48:01 +0530

    changing setup.py

--- setup.py
+++ setup.py
@@ -1,38 +1,3 @@
-[metadata]
-# replace with your username:
-name = en_audio2text-soumyade
-version = 1.0.1
-author = Soumya De
-author_email = cs.soumyade@gmail.com
-description = small wrapper around speech recognition package
-long_description = file: README.md
-long_description_content_type = text/markdown
-url = https://github.com/cssoumyade/en_audio2text
-project_urls =
-    Bug Tracker = https://github.com/cssoumyade/en_audio2text/issues
-install_requires = 
-	speech_recognition
-	pyaudio
-classifiers =
-        Development Status :: 2 - Pre-Alpha
-        Intended Audience :: Other Audience
-        Topic :: Education :: Testing
-        License :: OSI Approved :: MIT License
-        Programming Language :: Python :: 3
-        Programming Language :: Python :: 3.6
-
-[options]
-package_dir =
-    = src
-packages = find:
-python_requires = >=3.6
-
-[options.packages.find]
-where = src
-
-
-
-
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
@@ -52,7 +17,7 @@ setuptools.setup(
     },
     install_requires=[ 
 	speech_recognition,
-	pyaudio]
+	pyaudio],
     classifiers=[
 	"Development Status :: 2 - Pre-Alpha"
 	"Intended Audience :: Other Audience"
